1fba3f98c1279298f8ccdc85a56e9db75e3b6e34
Muggle Wei <mugglewei@gmail.com>
2023-10-09 00:52:27 +0800

    update cpp/gbenchmark/log_libs/pprof

--- cpp/gbenchmark/log_libs/pprof/mugglec_log_sync/mugglec_log_sync.cpp
+++ cpp/gbenchmark/log_libs/pprof/mugglec_log_sync/mugglec_log_sync.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <thread>
 #define MUGGLE_HOLD_LOG_MACRO 1
 #include "muggle/c/muggle_c.h"
 #include "log_msg.h"
@@ -7,13 +8,36 @@ int main()
 {
 	muggle_log_simple_init(-1, LOG_LEVEL_DEBUG);
 
+	int cnt = 10000;
 	std::vector<LogMsg> log_msgs;
-	GenLogMsgArray(100000, log_msgs);
+	GenLogMsgArray(cnt, log_msgs);
 
-	for (LogMsg &msg : log_msgs) {
-		LOG_INFO("u64: %llu, i64: %lld, u32: %lu, i32: %ld, s: %s",
-				 (unsigned long long)msg.u64, (long long)msg.i64,
-				 (unsigned long)msg.u32, (long)msg.i32, msg.s);
+	std::vector<std::thread> ths;
+	double elapsed_arr[16];
+	for (int i = 0; i < 16; i++) {
+		ths.push_back(std::thread([&log_msgs, &elapsed_arr, cnt, i] {
+			struct timespec ts1, ts2;
+			timespec_get(&ts1, TIME_UTC);
+
+			for (LogMsg &msg : log_msgs) {
+				LOG_INFO("u64: %llu, i64: %lld, u32: %lu, i32: %ld, s: %s",
+						 (unsigned long long)msg.u64, (long long)msg.i64,
+						 (unsigned long)msg.u32, (long)msg.i32, msg.s);
+			}
+
+			timespec_get(&ts2, TIME_UTC);
+			unsigned long elapsed = (ts2.tv_sec - ts1.tv_sec) * 1000000000 +
+									ts2.tv_nsec - ts1.tv_nsec;
+			elapsed_arr[i] = (double)elapsed / cnt;
+		}));
+	}
+
+	for (std::thread &th : ths) {
+		th.join();
+	}
+
+	for (int i = 0; i < 16; i++) {
+		printf("avg elapsed: %.3f ns\n", elapsed_arr[i]);
 	}
 
 	return 0;
--- cpp/gbenchmark/log_libs/pprof/spdlog_basic/spdlog_basic.cpp
+++ cpp/gbenchmark/log_libs/pprof/spdlog_basic/spdlog_basic.cpp
@@ -10,13 +10,36 @@ int main()
 	logger->set_pattern("%l|%E.%F|%s|%!|%t - %v");
 	spdlog::set_default_logger(logger);
 
+	int cnt = 10000;
 	std::vector<LogMsg> log_msgs;
-	GenLogMsgArray(100000, log_msgs);
+	GenLogMsgArray(cnt, log_msgs);
 
-	for (LogMsg &msg : log_msgs) {
-		SPDLOG_INFO("u64: {}, i64: {}, u32: {}, i32: {}, s: {}",
-					(unsigned long long)msg.u64, (long long)msg.i64,
-					(unsigned long)msg.u32, (long)msg.i32, msg.s);
+	std::vector<std::thread> ths;
+	double elapsed_arr[16];
+	for (int i = 0; i < 16; i++) {
+		ths.push_back(std::thread([&log_msgs, &elapsed_arr, cnt, i] {
+			struct timespec ts1, ts2;
+			timespec_get(&ts1, TIME_UTC);
+
+			for (LogMsg &msg : log_msgs) {
+				SPDLOG_INFO("u64: {}, i64: {}, u32: {}, i32: {}, s: {}",
+							(unsigned long long)msg.u64, (long long)msg.i64,
+							(unsigned long)msg.u32, (long)msg.i32, msg.s);
+			}
+
+			timespec_get(&ts2, TIME_UTC);
+			unsigned long elapsed = (ts2.tv_sec - ts1.tv_sec) * 1000000000 +
+									ts2.tv_nsec - ts1.tv_nsec;
+			elapsed_arr[i] = (double)elapsed / cnt;
+		}));
+	}
+
+	for (std::thread &th : ths) {
+		th.join();
+	}
+
+	for (int i = 0; i < 16; i++) {
+		printf("avg elapsed: %.3f ns\n", elapsed_arr[i]);
 	}
 
 	return 0;
